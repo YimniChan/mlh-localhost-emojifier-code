409554476294e5231e741833591c943576f210ed
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-29 15:29:30 -0400

    clone

---
+++ Downloads/mlh-localhost-emojifier-code-master/server/index.ts
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const request = require('request');
+const saveFace = require('./db');
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: '*',
+}));
+
+app.use(express.static(__dirname + "/../dist/"));
+
+const uriBase = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect/';
+const subscriptionKey = 'ed3ab614da804be1955bed0b600481ee';
+
+const port = 3000;
+
+app.post('/', (req, res) => {
+  const { imageUrl } = req.body;
+
+  const params = {
+    'returnFaceAttributes': 'emotion'
+  };
+
+  const options = {
+    url: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+  };
+  // TODO: Send Request to Face API
+  request.post(options, (error, respone, body)=>{
+    console.log(body);
+    res.setHerder('Content-Type', 'application/lson');
+    res.setd(body);
+  })
+  // TODO: Send Face API response to front-end
+  // TODO: Save Face API response to database
+});
+
+app.listen(port, () => console.log(`Emojifier back-end listening on port ${port}!`));
